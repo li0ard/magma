fd34ca0b1926ce3dddf107150ab8cfb5ed59b7fd
Lizard <li0ard@proton.me>
2025-07-24 01:22:49 +0600

    docs: add jsdoc for `kexp15` and `kimp15`

--- src/modes/kexp.ts
+++ src/modes/kexp.ts
@@ -1,7 +1,14 @@
 import { BLOCK_SIZE, Magma } from "../";
 import { kexp15 as kexp15_, kimp15 as kimp15_ } from "@li0ard/gost3413";
 
-export const kexp15 = (key: Uint8Array, keyEnc: Uint8Array, keyMac: Uint8Array, iv: Uint8Array) => {
+/**
+ * KExp15 key exporting
+ * @param key Key to export
+ * @param keyEnc Key for key encryption
+ * @param keyMac Key for key authentication
+ * @param iv Initialization vector (Half of block size)
+ */
+export const kexp15 = (key: Uint8Array, keyEnc: Uint8Array, keyMac: Uint8Array, iv: Uint8Array): Uint8Array => {
     const keyCipher = new Magma(keyEnc);
     const keyEncrypter = (block: Uint8Array) => keyCipher.encryptBlock(block);
     const macCipher = new Magma(keyMac);
@@ -9,7 +16,14 @@ export const kexp15 = (key: Uint8Array, keyEnc: Uint8Array, keyMac: Uint8Array, 
     return kexp15_(keyEncrypter, macEncrypter, BLOCK_SIZE, key, iv);
 }
 
-export const kimp15 = (kexp: Uint8Array, keyEnc: Uint8Array, keyMac: Uint8Array, iv: Uint8Array) => {
+/**
+ * KImp15 key importing
+ * @param kexp Key to import
+ * @param keyEnc Key for key decryption
+ * @param keyMac Key for key authentication
+ * @param iv Initialization vector (Half of block size)
+ */
+export const kimp15 = (kexp: Uint8Array, keyEnc: Uint8Array, keyMac: Uint8Array, iv: Uint8Array): Uint8Array => {
     const keyCipher = new Magma(keyEnc);
     const keyEncrypter = (block: Uint8Array) => keyCipher.encryptBlock(block);
     const macCipher = new Magma(keyMac);
